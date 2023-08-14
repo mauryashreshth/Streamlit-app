a3d5f5a9736002591621a876c589beb88b9ce8fc
mauryashreshth <shreshthmaurya2003@gmail.com>
2023-08-14 22:34:15 +0530

    Update

--- main.py
+++ main.py
@@ -2,72 +2,63 @@ import streamlit as st
 from streamlit_chat import message
 from bardapi import Bard
 import os
-os.environ['_BARD_API_KEY']="YggvfGCbgwKg-Gj_1snZ6QD7QqSipEx4deG6ueqtjvIZvXLU3bBjeqiWKPsGP5sRcXzu2A."
-# set your __Secure-1PSID value to key
-# dilKiBaat = 'YggvfJQF5xswQVjlKzOIGg5AskxEm6j3POo9E9g8lMFVh2EfEWcOpuZxxymNu6wZVuPHmg.'
 
-st.set_page_config(page_title="AI BOT")
+os.environ['_BARD_API_KEY'] = "ZwgvfP37iHbsWxdcX1blTpP-bds7wjQRympBGfidWTmc4VQe4B5I82_qqSvz_KihLfaxiQ."
+
+# set your __Secure-1PSID value to key
+
+st.set_page_config(page_title="Personal AI Chatbot")
 st.markdown(
     '''
-    <html>
-        <head>
-            <title>AI BOT</title>
-        </head>
-    </html>
     <style>
         [data-testid="stAppViewContainer"] {
             background-image: url("https://c4.wallpaperflare.com/wallpaper/306/908/890/spots-background-light-dark-wallpaper-preview.jpg");
             background-size: cover;
         }
         .body {
-            background-color: transparent !important;
+            background-color: rgba(0, 0, 0, 0) !important;
         }
     </style>
     ''',
     unsafe_allow_html=True,
 )
 
-st.title("Tutoring Bot")
+st.title("Your Personal AI Chatbot!")
 
 
 # Function to generate a response
-def genrateRes(prompt):
-    
+def generate_response(prompt):
     # set your input text
     input_text = prompt
 
     # Send an API request and get a response.
-    bard = Bard(timeout=10)  # Set timeout in seconds
-    response = bard.get_answer(prompt)['content']
+    response = Bard().get_answer(prompt)['content']
     return response
 
 
 def get_text():
-    input_text = st.text_input("Puneet Bot: ", "", key="input")
+    input_text = st.text_input("Made by Shreshth Maurya, NSUT", "", key="input")
     return input_text
 
 
 # Creating two lists to store generated and past texts
-
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
 
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 
-
 # Texting phase
 user_input = get_text()
 
 if user_input:
     print(user_input)
-    output = genrateRes(user_input)
+    output = generate_response(user_input)
     print(output)
     st.session_state['past'].append(user_input)
     st.session_state['generated'].append(output)
 
-if st.session_state['generated']:
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state['generated'][i], key = 'generated_'+str(i)) # We use message to display the generated message in reverse order. The key is used to store unique item
-        message(st.session_state['past'][i], key = 'past_'+str(i), is_user=True) # We use message to display the past message in reverse order. The key is used to mark it as unique
-
+if 'generated' in st.session_state and 'past' in st.session_state:
+    for i in range(len(st.session_state['generated']) - 1, -1, -1):
+        message(st.session_state['generated'][i], key='generated_' + str(i))
+        message(st.session_state['past'][i], key='past_' + str(i), is_user=True)
